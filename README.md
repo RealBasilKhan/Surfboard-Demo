2193e579bc9ecff913bff5702556d95187d924ca
Basil Khan <basilkhan76@gmail.com>
2022-02-04 19:10:48 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+To install the app, simply run npm install.
+
+Create a ChatEngine account and update the project id.
+
+Enjoy chatting!
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "688728e8-8053-4878-a0f5-8418fb7be63e";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
