9f2cdf57f352ea77ba16c607513f100bf2042fbe
Basil Khan <basilkhan76@gmail.com>
2022-03-07 09:40:33 -0500

    updated env 2

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const projectID = PROJECT_ID;
+const projectID = process.env.PROJECT_ID;
 
 const Modal = () => {
   const [username, setUsername] = useState("");
